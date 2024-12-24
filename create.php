c4abb91297ea5eca894b7c0cc50b0ea09d032c89
Tamer <62846154+Tamer3mansor@users.noreply.github.com>
2024-12-24 20:46:45 +0200

    improved

--- create.php
+++ create.php
@@ -53,11 +53,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <body>
     <form method="post" enctype="multipart/form-data">
-        <input type="text" name="name" id="name" value="<?= isset($_POST['name']) ? $_POST['name'] : '--' ?>" /><?php if (in_array("name", $error_fields))
-                  echo "Enter the name" ?>
+        <input type="text" name="name" id="name" placeholder="name"
+            value="<?= isset($_POST['name']) ? $_POST['name'] : '' ?>" /><?php if (in_array("name", $error_fields))
+                      echo "Enter the name" ?>
             <br>
             <input type="email" id="email" placeholder="Email" name="email"
-                value="<?= isset($_POST['email']) ? $_POST['email'] : '--' ?>"><?php if (in_array("email", $error_fields))
+                value="<?= isset($_POST['email']) ? $_POST['email'] : '' ?>"><?php if (in_array("email", $error_fields))
                           echo "Enter the email" ?><br>
             <input type="password" id="password" placeholder="Password" name="password"><?php if (in_array("password", $error_fields))
                           echo "Enter password high than 5 char" ?><br>
