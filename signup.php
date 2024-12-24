dccf2e18a902e1bd23fbe3bb537f9db6a948d1df
Tamer <62846154+Tamer3mansor@users.noreply.github.com>
2024-12-24 21:45:39 +0200

    .

--- signup.php
+++ signup.php
@@ -1,38 +1,73 @@
 <?php
-$error_arr = [];
-if (isset($_GET['error'])) {
-  $error_arr = explode(",", $_GET['error']);
+$error_fields = [];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if (!(isset($_POST["name"]) && !empty($_POST["name"]))) {
+    $error_fields[] = "name";
+  }
+  if (!(isset($_POST["email"]) && filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL))) {
+    $error_fields[] = "email";
+  }
+  if (!(isset($_POST["password"]) && strlen($_POST["password"]) > 5)) {
+    $error_fields[] = "password";
+  }
+
+  if (!$error_fields) {
+    $conn = mysqli_connect("localhost", "root", "", "php");
+    if (!$conn) {
+      echo mysqli_connect_error();
+      exit;
+      // die("Connection error: " . mysqli_connect_error());
+    }
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = sha1($_POST['password']);
+    $admin = isset($_POST['admin']) ? 1 : 0;
+    $gender = isset($_POST['gender']) ? $_POST['gender'] : null;
+    $avatar = "";
+    if ($_FILES['avatar']['error'] == UPLOAD_ERR_OK) {
+      $upload_dir = $_SERVER['DOCUMENT_ROOT'] . '/project/uploads';
+      $tmp_dir = $_FILES['avatar']['tmp_name'];
+      $img_name = basename($_FILES['avatar']['name']);
+      move_uploaded_file($tmp_dir, "$upload_dir/$name.$img_name");
+    } else {
+      echo $_FILES['avatar']['error'];
+    }
+    $query = "INSERT INTO `users` (`name`, `pass`, `email`, `gender`, `admin`) 
+                  VALUES ('$name', '$password', '$email', '$gender', 0)";
+
+    if (mysqli_query($conn, $query)) {
+      header("Location:list_user.php");
+      exit;
+    } else {
+      echo "Database error: " . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+  }
+
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-</head>
+<html>
 
 <body>
-  <form method="post" action="process.php">
-    <label for="name">Name</label> <br />
-    <input type="text" id="1" name="name" required /><?php if (in_array("name", $error_arr))
-      echo "enter name"; ?><br />
-    <label for="password">password</label> <br />
-    <input type="password" id="2" name="password"
-      required /><?php if (in_array("password", $error_arr))
-        echo "enter name"; ?><br />
-    <label for="email">email</label><br />
-    <input type="email" id="3" name="email"
-      required /><?php if (in_array("email", $error_arr))
-        echo "enter name"; ?><br />
-    <label for="gender">gender</label><br />
-    <input type="radio" id="4" name="gender" value="male" required />male<br />
-    <input type="radio" id="5" name="gender" value="female" required />female<br />
-    <input type="submit" id="6" name="submit" value="Done" required /><br />
-  </form>
-  <a href="login.php">Have an account ? Log IN</a>
-</body>
+  <form method="post" enctype="multipart/form-data">
+    <input type="text" name="name" id="name" placeholder="name"
+      value="<?= isset($_POST['name']) ? $_POST['name'] : '' ?>" /><?php if (in_array("name", $error_fields))
+              echo "Enter the name" ?>
+      <br>
+      <input type="email" id="email" placeholder="Email" name="email"
+        value="<?= isset($_POST['email']) ? $_POST['email'] : '' ?>"><?php if (in_array("email", $error_fields))
+                echo "Enter the email" ?><br>
+      <input type="password" id="password" placeholder="Password" name="password"><?php if (in_array("password", $error_fields))
+                echo "Enter password high than 5 char" ?><br>
+      Male <input type="radio" id="male" name="gender" value="male" required> <br>
+      Female <input type="radio" id="female" name="gender" value="female" required><br>
+      <!-- Admin <input type="checkbox" name="admin"><br> -->
+      <input type="file" value="file" name="avatar"><br>
+      <input type="submit" value="add user">
+    </form>
+  </body>
 
-</html>
+  </html>
